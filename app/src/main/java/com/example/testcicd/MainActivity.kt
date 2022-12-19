61644c13a7347cbb7d12d8b38821a9bf81a4b7aa
selvan7 <777tamil777@gmail.com>
2022-12-19 13:18:21 +0530

    Feat: Toast is Added

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.testcicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "Test CI/CD", Toast.LENGTH_LONG).show()
     }
 }
